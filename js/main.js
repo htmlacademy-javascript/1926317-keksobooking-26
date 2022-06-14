8643fce3435e46a6d20baf94625191f11dc2aeb8
Andrey <rizo156@mail.ru>
2022-06-14 22:00:17 +0500

    Дз4

--- js/main.js
+++ js/main.js
@@ -1,28 +1,66 @@
 // Функция, возвращающая случайное целое число из диапазона
-const getRandomValue = function(minValue, maxValue) {
-  if (minValue >= 0 && minValue < maxValue) {
-    return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+const getRandomPositiveInteger = function (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+// Функция,возвращающая случайное число с плавающей точкой из диапазона
+const getRandomPositiveFloat = function (a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+};
+// Функция,возвращающая случайный элемент массива
+const getRandomArrayElement = function(elements) {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+};
+// Функция массива случайной длины
+const getRandomArrayLength = function (array) {
+  const lengthArray = getRandomPositiveInteger(0,array.length);
+  const newArray = [];
+  for (let i = 0; i < lengthArray; i++) {
+    newArray.push(array[i]);
   }
-  else {
-    if (minValue<0) {return 'Введите положительные числа';}
-    else {
-      if  (minValue>=maxValue) {return 'Начальное значение должно быть меньше конечного';}
+  return newArray;
+};
+// Переменные
+const TITLE = ['Сдам дешево','В хорошие руки','Для молодожен','Отдыхайте весело'];
+const PRICE = [1000,2000,5000,7772];
+const TYPE = ['palace','flat','house','bungalow','hotel'];
+const ROOMS = [1,2,3,4];
+const GUESTS = [1,2,3];
+const CHECKIN = ['12:00', '13:00', '14:00'];
+const CHECKOUT = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DISCRIPTION = ['отличный вид', 'в центре города'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+//Создание объекта
+const creatObject = function () {
+  return {
+    author:{
+      avatar:''
+    },
+    offer:{
+      title:getRandomArrayElement(TITLE),
+      address:'',
+      price:getRandomArrayElement(PRICE),
+      type:getRandomArrayElement(TYPE),
+      rooms:getRandomArrayElement(ROOMS),
+      guests:getRandomArrayElement(GUESTS),
+      checkin:getRandomArrayElement(CHECKIN),
+      checkout:getRandomArrayElement(CHECKOUT),
+      features:getRandomArrayLength(FEATURES),
+      description:getRandomArrayElement(DISCRIPTION),
+      photos:getRandomArrayLength(PHOTOS)
+    },
+    location:{
+      lat:getRandomPositiveFloat(35.65000,35.70000,5),
+      lng:getRandomPositiveFloat(139.70000,139.80000,5)
     }
-  }
+  };
 };
 
-getRandomValue (10,22);
-
-const getRandomValueFloat = function(minValue, maxValue, float) {
-  if (minValue >= 0 && minValue < maxValue) {
-    return (Math.random() * (maxValue - minValue) + minValue).toFixed(float);
-  }
-  else {
-    if (minValue<0) {return 'Введите положительные числа';}
-    else {
-      if  (minValue>=maxValue) {return 'Начальное значение должно быть меньше конечного';}
-    }
-  }
-};
-
-getRandomValueFloat (1.1, 1.2, 3);
+creatObject();
